a545effb3dcc2125ab1a997ca42177667bfb05bc
Mariusz Galek <MariuszGalek@users.noreply.github.com>
2020-06-04 16:18:22 +0200

    Add check text length and passwords functions

--- js/main.js
+++ js/main.js
@@ -6,9 +6,55 @@ const sendBtn = document.querySelector('.send');
 const clearBtn = document.querySelector('.clear');
 const popup = document.querySelector('.popup');
 
+const showError = (input, msg) => {
+    const formBox = input.parentElement;
+    const errorMsg = formBox.querySelector('.error-text');
+
+    formBox.classList.add('error');
+    errorMsg.textContent = msg;
+}
+
+const clearError = input => {
+    const formBox = input.parentElement;
+    formBox.classList.remove('error');
+}
+
+const checkForm = input => {
+    input.forEach(el => {
+        if(el.value === '') {
+            showError(el, el.placeholder)
+        } else {
+            clearError(el);
+        }
+    })
+}
+
+const checkLength = (input, min) => {
+    if (input.value.length < min) {
+        showError(input, `${input.previousElementSibling.innerText.slice(0, -1)} contains at least ${min} chars.`)
+    }
+}
+
+const checkPassword = (pass1, pass2) => {
+    if (pass1.value !== pass2.value) {
+        showError(pass2, "Passwords don't match!");
+    }
+}
+
 clearBtn.addEventListener('click', e => {
     e.preventDefault();
     [username, pass, pass2, email].forEach(el => {
         el.value = '';
+        clearError(el);
     })
+})
+
+sendBtn.addEventListener('click', e => {
+    e.preventDefault();
+
+    checkForm([username, pass, pass2, email])
+    checkLength(username, 3);
+    checkLength(pass, 8);
+    checkPassword(pass, pass2);
+
 })
